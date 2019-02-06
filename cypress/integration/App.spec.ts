26c892c041712fd069b5ade443cd17f42f551b21
Grzegorz Moskal <g.moskal@gmail.com>
2019-02-06 16:04:07 +0100

    e2e tests postponed

--- cypress/integration/App.spec.ts
+++ cypress/integration/App.spec.ts
@@ -2,7 +2,7 @@
 ///<reference path="../../node_modules/cypress/types/index.d.ts"/>
 
 describe("App", () => {
-    it("cy.get() - query DOM elements", () => {
+    it.skip("cy.get() - query DOM elements", () => {
         cy.visit("http://localhost:4000")
         cy.get("#app .Title").contains("Route")
     })
